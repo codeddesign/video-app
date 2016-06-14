2419b0cdc25d3b27f8230ff74b2f70e167fb37ce
Ovidiu B <boovad@live.com>
2016-06-15 01:01:18 +0300

    removed info-messages (everywhere)

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -92,41 +92,7 @@
 
                 toastr['error'](msg); // Wire up an event handler to a button in the toast, if it exists
 
-            <?php } else if (isset($success)) {?>
-                var getMessage = function () {
-
-                    var msg = '<?=$success?>';
-                    return msg;
-                };
-
-                var msg = "";
-                var title = "";
-
-                var toastIndex = toastCount++;
-
-                toastr.options = {
-                    closeButton: true,
-                    debug: true,
-                    progressBar: true,
-                    preventDuplicates: true,
-                    positionClass: "toast-top-right",
-                    onclick: null
-                };
-
-                toastr.options.showDuration = 1000;
-                toastr.options.hideDuration = 1000;
-                toastr.options.timeOut = 7000;
-                toastr.options.extendedTimeOut = 1000;
-                toastr.options.showEasing = "swing";
-                toastr.options.hideEasing = "linear";
-                toastr.options.showMethod = "fadeIn";
-                toastr.options.hideMethod = "fadeOut";
-                if (!msg) {
-                    msg = getMessage();
-                }
-
-                toastr['info'](msg);
-            <?php }?>
+            <?php } ?>
      });
 </script>
 </body>
