3e98fbddd9e083886bd9a24ddbed324306d95487
codeddesign <web@codeddesign.org>
2016-06-16 14:58:46 -0400

    stupid boostrap messages now end

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,55 +4,6 @@
 <!-- SPARKLINE DATA CHART -->
 <script src="{{url('/')}}/template/js/sparkline.min.js"></script>
 <script src="{{url('/')}}/template/js/sparkline.js"></script>
-<!-- TOASTR SCRIPT -->
-<script src="{{url('/')}}/template/js/toastr.min.js"></script>
-
-<script>
-
-    $(document).ready(function(){
-
-
-            var i = -1;
-            var toastCount = 0;
-
-            <?php if(isset($error)){ ?>
-                var getMessage = function () {
-
-                    var msg = '<?=$error?>';
-                    return msg;
-                };
-
-                var msg = "";
-                var title = "";
-
-                var toastIndex = toastCount++;
-
-                toastr.options = {
-                    closeButton: true,
-                    debug: true,
-                    progressBar: true,
-                    preventDuplicates: true,
-                    positionClass: "toast-top-right",
-                    onclick: null
-                };
-
-                toastr.options.showDuration = 1000;
-                toastr.options.hideDuration = 1000;
-                toastr.options.timeOut = 7000;
-                toastr.options.extendedTimeOut = 1000;
-                toastr.options.showEasing = "swing";
-                toastr.options.hideEasing = "linear";
-                toastr.options.showMethod = "fadeIn";
-                toastr.options.hideMethod = "fadeOut";
-                if (!msg) {
-                    msg = getMessage();
-                }
-
-                toastr['error'](msg); // Wire up an event handler to a button in the toast, if it exists
-
-            <?php } ?>
-     });
-</script>
 
 </body>
 </html>
