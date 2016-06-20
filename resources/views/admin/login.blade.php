bf30e32faecc321bb0d082382b0d62c0524dc1c3
Ovidiu B <boovad@live.com>
2016-06-21 01:29:57 +0300

    Removed toastr residues

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -12,8 +12,6 @@
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <!-- include ingrid lib -->
 <script type="text/javascript" src="/template/js/jquery.ingrid.js"></script>
-<!-- Toastr style -->
-<link href="/template/css/toastr.min.css" rel="stylesheet">
 
 <!-- include typekit font -->
 <script src="https://use.typekit.net/lwk5wec.js"></script>
@@ -48,52 +46,5 @@
     </div>
 </div>
 
-<!-- Toastr script -->
-<script src="/template/js/toastr.min.js"></script>
-
-<script>
-
-    $(document).ready(function(){
-            var i = -1;
-            var toastCount = 0;
-
-            <?php if (isset($error)) {?>
-                var getMessage = function () {
-
-                    var msg = '<?=$error?>';
-                    return msg;
-                };
-
-                var msg = "";
-                var title = "";
-
-                var toastIndex = toastCount++;
-
-                toastr.options = {
-                    closeButton: true,
-                    debug: true,
-                    progressBar: true,
-                    preventDuplicates: true,
-                    positionClass: "toast-top-right",
-                    onclick: null
-                };
-
-                toastr.options.showDuration = 1000;
-                toastr.options.hideDuration = 1000;
-                toastr.options.timeOut = 7000;
-                toastr.options.extendedTimeOut = 1000;
-                toastr.options.showEasing = "swing";
-                toastr.options.hideEasing = "linear";
-                toastr.options.showMethod = "fadeIn";
-                toastr.options.hideMethod = "fadeOut";
-                if (!msg) {
-                    msg = getMessage();
-                }
-
-                toastr['error'](msg); // Wire up an event handler to a button in the toast, if it exists
-
-            <?php } ?>
-     });
-</script>
 </body>
 </html>
--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -13,9 +13,6 @@
 <!-- include ingrid lib -->
 <script type="text/javascript" src="{{url('/')}}/template/js/jquery.ingrid.js"></script>
 
-<!-- Toastr style -->
-<link href="{{url('/')}}/template/css/toastr.min.css" rel="stylesheet">
-
 <!-- include typekit font -->
 <script src="https://use.typekit.net/lwk5wec.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
@@ -51,54 +48,5 @@
         <div class="login-lostpassbutton"><a href="/account/recover">LOST PASSWORD</a></div>
     </div>
 </div>
-<!-- Toastr script -->
-<script src="{{url('/')}}/template/js/toastr.min.js"></script>
-
-<script>
-
-    $(document).ready(function(){
-
-
-            var i = -1;
-            var toastCount = 0;
-
-            <?php if (isset($error)) {?>
-                var getMessage = function () {
-
-                    var msg = '<?=$error?>';
-                    return msg;
-                };
-
-                var msg = "";
-                var title = "";
-
-                var toastIndex = toastCount++;
-
-                toastr.options = {
-                    closeButton: true,
-                    debug: true,
-                    progressBar: true,
-                    preventDuplicates: true,
-                    positionClass: "toast-top-right",
-                    onclick: null
-                };
-
-                toastr.options.showDuration = 1000;
-                toastr.options.hideDuration = 1000;
-                toastr.options.timeOut = 7000;
-                toastr.options.extendedTimeOut = 1000;
-                toastr.options.showEasing = "swing";
-                toastr.options.hideEasing = "linear";
-                toastr.options.showMethod = "fadeIn";
-                toastr.options.hideMethod = "fadeOut";
-                if (!msg) {
-                    msg = getMessage();
-                }
-
-                toastr['error'](msg); // Wire up an event handler to a button in the toast, if it exists
-
-            <?php } ?>
-     });
-</script>
 </body>
 </html>
