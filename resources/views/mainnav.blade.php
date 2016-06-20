e4dd95d40db607b85413e4ac41079835cd7b3120
Ovidiu B <boovad@live.com>
2016-06-21 01:35:43 +0300

    removed mainnav & welcome: not being used

--- resources/views/mainnav.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- PURPLE NAV AREA -->
-<div class="rightside-nav">
-	<div class="rightside-navlefttitle">EDIT ACCOUNT</div>
-	<div class="rightside-navdropdown">ACCOUNT DETAILS <span></span></div>
-	<ul class="rightside-navdroparea" style="display:none;">
-		<li>Edit Account</li>
-		<li>Logout</li>
-	</ul>
-</div>
