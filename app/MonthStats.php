eec2f7e3047e6306c4e09b5ea5c3f27386d13aa3
codeddesign <web@codeddesign.org>
2016-06-29 13:17:49 -0400

    Added handle for missing ids

--- app/MonthStats.php
+++ app/MonthStats.php
@@ -162,6 +162,13 @@ class MonthStats
                 });
 
             foreach ($grouped as $key => $value) {
+		if (!isset($data['list']['plays'][$key])) {
+                    $data['list'] = [
+                        'plays' => [$key => 0],
+                        'revenue' => [$key => 0],
+                    ];
+                }
+
                 $data['list']['plays'][$key] += $value;
                 $data['list']['revenue'][$key] += $value * $this->CENTS;
             }
