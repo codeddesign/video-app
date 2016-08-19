2088e5232d6ff38bd6480d70ad3310e621eac151
codeddesign <web@codeddesign.org>
2016-08-19 16:00:46 -0400

    Set Cors max-age to 0

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,6 +17,7 @@ class Cors
     public function handle($request, Closure $next) {
           return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Max-Age', 0)
             ->header('Access-Control-Allow-Credentials', 'false')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Accept, Authorization');
