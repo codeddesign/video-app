a8a08e3c2ebcc083e2ca25c0b23bb5ea7f413bb8
codeddesign <web@codeddesign.org>
2016-08-19 14:39:16 -0400

    Change cors headers

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -14,19 +14,11 @@ class Cors
      *
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-        $headers = [
-            'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => 'POST, GET',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Requested-With, Accept',
-        ];
-
-        $response = $next($request);
-        foreach ($headers as $key => $value) {
-            $response->header($key, $value);
-        }
-
-        return $response;
+    public function handle($request, Closure $next) {
+          return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Credentials', 'false')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Accept, Authorization');
     }
 }
